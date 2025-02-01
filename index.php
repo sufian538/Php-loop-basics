b9380a26f46b614bf2db7214e3ac06d0dbcafe8c
TURK225 <turkfarooq445@gmail.com>
2025-01-31 20:58:03 -0800

    done

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+// echo "Hello";
+// $name = "Sufian";
+// echo $name."<br>";
+// print_r($name);
+
+// var_dump($name);
+// echo "<br>";
+// $a = 8;
+// $b = 6.5;
+
+// var_dump($a);
+// echo "<br>";
+
+// var_dump($b);
+// echo "<br>";
+
+
+// $age = 19.6;
+// echo (round($age));
+// echo "<br>";    
+
+// $arr = [89,6,3,45];
+// echo (min($arr));
+// echo "<br>";    
+
+
+// $a = pi();
+// echo ($a);
+// echo "<br>";    
+
+// $arr = [5,9,25,22];
+// echo (max($arr));
+// echo "<br>";  
+
+// $arr = [5,90,25,22];
+// echo (min($arr));
+// echo "<br>";  
+
+// $h = rand();
+// echo ($h);
+
+$a = "monday";
+switch($a)
+{
+    case "monday":
+        echo ($a."<br>"); 
+        break; 
+        case "tuesday":
+            echo ($a."<br>"); 
+            break;
+            case "wednesday":
+                echo ($a."<br>"); 
+                default:
+                echo("your number is incorrect");
+    
+}
+?>
